a0124505a39141d83b69af3a4faa14fe5c0b1a69
Jerry Boateng <jerrito0240@gmail.com>
2022-10-14 15:51:16 +0000

    first commit

---
+++ src/main/java/com/mycompany/mtn_app/MTN_APP.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mtn_app;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author jerrito
+ */
+public class MTN_APP {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Testing Joptionpane", "Momo",
+                JOptionPane.INFORMATION_MESSAGE);
+      String h=  JOptionPane.showInputDialog(null,"What is your name","Pin"
+                ,JOptionPane.INFORMATION_MESSAGE);
+        System.out.println(h);
+    }
+}
+//115.21 - 2
+//48.1- 1
+//471.70 -3
+//483.68 - 4
+//423.22 -3.5
